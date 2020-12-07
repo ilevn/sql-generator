3b5552eddca3ce6a0cffdafe3b9e052707b0df02
ilevn <nilsntth@gmail.com>
2020-12-07 17:06:54 +0100

    Rework generator base class - Custom generator support for columns and data types - Useful non data-type converters now reside in extra_generators

--- sql_generator/__init__.py
+++ sql_generator/__init__.py
@@ -1,3 +1,3 @@
 from .analyser import Analyser
+from .data_type_generators import *
 from .generator import Generator, write_results_to_file, flatten_dict_list
-from .generators import *
--- sql_generator/generator.py
+++ sql_generator/generator.py
@@ -1,11 +1,11 @@
 import random
 from collections import defaultdict
+from datetime import datetime
 
 import toposort
 
 from .analyser import Analyser
-from .generators import password_generator, first_name_generator, last_name_generator, text_generator, email_generator, \
-    phone_generator, get_converter
+from .data_type_generators import get_generator
 
 
 def flatten_dict_list(table_statements):
@@ -23,7 +23,6 @@ def write_results_to_file(statements, dest="output.sql", should_truncate=False, 
         pre_face = "\n".join(f"TRUNCATE TABLE {table} RESTART IDENTITY CASCADE;" for table in statements)
     statements = flatten_dict_list(statements)
 
-    from datetime import datetime
     now = format(datetime.now(), "%b %d %Y at %H:%M:%S")
     with open(dest, "w") as f:
         f.write("-- GENERATED AUTOMATICALLY. DO NOT ALTER THESE MANUALLY! --\n")
@@ -33,33 +32,21 @@ def write_results_to_file(statements, dest="output.sql", should_truncate=False, 
 
 
 class Generator:
-    def __init__(self, connection, password_tables=None, custom_converters=None):
+    def __init__(self, connection, data_type_generators=None, column_generators=None, statement_prefix=""):
         self.analyser = Analyser(connection)
-        self.password_tables = password_tables or ()
-        self.converters = {
-            'password_hash': password_generator,
-            'first_name': first_name_generator,
-            'last_name': last_name_generator,
-            'character varying': text_generator,
-            'email': email_generator,
-            'phone': phone_generator,
-            **(custom_converters or {})
-        }
+        self.statement_prefix = statement_prefix
+        # Custom generators for data types and columns.
+        self.data_type_generators = data_type_generators or {}
+        self.column_generators = column_generators
         # Table references for foreign key relations.
         self.id_refs = defaultdict(list)
         self.tables = [self.analyser.get_table_info(table) for table in
                        toposort.toposort_flatten(self.analyser.generate_dependency_graph())]
 
-    def format_insert_statement_for_row(self, table, data):
-        """INSERT INTO table (...) VALUES (...)"""
+    def format_sql_statement_for_row(self, table, data):
         columns = ", ".join(data.keys())
         values = ", ".join(map(repr, data.values()))
-        extra = ""
-        if table.name in self.password_tables:
-            # Display their clear-text password.
-            extra = f"\n-- Clear-text password: {data['password_hash'].extra} --\n"
-
-        return f"{extra}INSERT INTO {table.name} ({columns}) OVERRIDING SYSTEM VALUE VALUES ({values});"
+        return f"{self.statement_prefix}INSERT INTO {table.name} ({columns}) OVERRIDING SYSTEM VALUE VALUES ({values});"
 
     def generate_single_table_data(self, table, curr_id=1):
         data = {}
@@ -87,25 +74,30 @@ class Generator:
         return data
 
     def generate_column_data(self, column):
+        d_type = column.data_type.lower()
         try:
-            # Check for special converters first.
-            converter = self.converters.get(column.name) or self.converters[column.data_type]
+            # Check for special generators first.
+            converter = self.column_generators.get(column.name) or self.data_type_generators[d_type]
         except KeyError:
-            converter = get_converter(column.data_type)
+            try:
+                converter = get_generator(d_type)
+            except KeyError:
+                print(f"Unsupported data type `{d_type}` for column `{column.name}`.")
+                print("Please use a custom generator for this.")
+                # Make pycharm happy.
+                return exit(1)
 
         return converter(column)
 
     def generate_table_data(self, table, amount=1):
-        # Manually keep track of our IDs.
-        i = 1
-        data = []
-        while i < amount + 1:
-            data.append(self.generate_single_table_data(table, i))
-            i += 1
+        formatted_data = []
+        for table_id in range(1, amount + 1):
+            table_data = self.generate_single_table_data(table, table_id)
+            formatted_data.append(self.format_sql_statement_for_row(table, table_data))
 
-        formatted_data = [self.format_insert_statement_for_row(table, d) for d in data]
         if table.has_id_column:
-            formatted_data.append(f"ALTER SEQUENCE {table}_id_seq RESTART WITH {i};")
+            next_id = len(formatted_data) + 1
+            formatted_data.append(f"ALTER SEQUENCE {table}_id_seq RESTART WITH {next_id};")
 
         formatted_data[-1] = formatted_data[-1] + "\n"
         return formatted_data
